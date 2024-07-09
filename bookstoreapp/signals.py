9f8ea3fceabe026bc29995f94d3a10e3ffbb08d7
vishnu.vp <vishnu.vp@scrapehero.com>
2024-07-09 15:14:28 +0530

    add logs in signal

--- bookstoreapp/signals.py
+++ bookstoreapp/signals.py
@@ -1,9 +1,13 @@
+import logging
+
 from django.conf import settings
 from django.core.mail import send_mail
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from .models import User
+
+logger = logging.getLogger(__name__)
 
 
 @receiver(post_save, sender=User)
@@ -17,5 +21,5 @@ def send_email_to_user(sender, instance, created, **kwargs):
             from_email = settings.DEFAULT_FROM_EMAIL
             to_email = instance.email
             send_mail(subject, message, from_email, [to_email])
-    except Exception:
-        pass
+    except Exception as e:
+        logger.error(f"Error sending email to {instance.username}: {e}")
